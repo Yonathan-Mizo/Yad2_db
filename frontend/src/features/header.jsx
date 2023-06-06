7d1dd7938cf2e7167a8c32ab9fa5d02213d14d45
Yonathan Mizo <ralteyonatan@yesmalot.co.il>
2023-06-06 20:32:22 +0300

    New Commit for Frontend

--- frontend/src/features/header.jsx
+++ frontend/src/features/header.jsx
@@ -1,44 +1,31 @@
-import React from 'react'
-import {BiUser, BiHeart, BiBell} from 'react-icons/bi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { headerItems } from "./HeaderItems";
+import "./Header.css";
 
 export default function Header() {
     return(
+        <header className="header">
+            <ul className="header-items">
+          {headerItems.map((item) => {
+             return (
+                <li key={item.id} className={item.cName}>
+                  <Link to={item.path}>{item.title}</Link>
+                </li>
+              )
+          })}
+          </ul>
+          <div className="tag-item">
+          <Link to="vihicle-price-list" className="link">
+      <img className="tag" src="car.png" alt="car-tag"/>
+      <span>Vihicle Price List</span>
+    </Link> 
+    <Link to="convention" className="link">
+      <img className="tag price-tag" src="price-tag.png" alt="price-tag"/>
+      <span>Convention</span>
+    </Link> 
+    </div>
+        </header>
         
-        <div className='items'>
-        <img id="logo" src="https://upload.wikimedia.org/wikipedia/he/thumb/4/42/Yad2_logo.svg/1200px-Yad2_logo.svg.png" alt="yad2 Logo"></img>
-        <div className='wrapper'> 
-        <p>real estate</p>
-        <div className='more-items'>
-        <div className='item-content'>
-        <a class="link" href="#">Homes for Sale</a>
-        <a class="link" href="#">Appartment for Rent</a>
-        <a class="link" href="#">Partners</a>
-        <a class="link" href="#">commercial</a>
-        <a class="link" href="#">search on a map</a>
-        <a class="link" href="#">Hand 1 new appartments</a>
-        </div>
-        <div className='item-content'>
-        <a class="link" href="#">Daron - The profetioanal assiatant</a>
-        <a class="link" href="#">receiver</a>
-        <a class="link" href="#">property valuation</a>
-        <a class="link" href="#">Real estate agencies in israel</a>
-
-        </div>
-        </div>
-        </div>
-            
-            {/* <p>vihicle</p>
-            <p>second hand</p>
-            <p>IL needed</p>
-            <p>Bussiness for sale</p>
-            <p>pets</p>
-            <p>more</p>
-        
-        <BiUser className='icon'/>
-        <BiHeart className='icon'/>
-        <BiBell className='icon'/>
-        
-        <button>+ publish a new app</button> */}
-        </div>
     )
 }
