ae1afb7e63811d5a68e6cedac016c9a085c96928
Yonathan Mizo <ralteyonatan@yesmalot.co.il>
2023-06-21 20:54:29 +0300

    App and index update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,38 @@
-import React from "react";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./features/Navbar";
-//import { Header } from "antd/es/layout/layout";
-import Header from "./features/Header"
-// import Home from "./pages/Home";
-// import Consulting from "./pages/Consulting";
-// import Contactus from "./pages/Contactus";
-// import Design from "./pages/Design";
-// import Development from "./pages/Development";
-// import Marketing from "./pages/Marketing";
-// import Products from "./pages/Products";
-// import Services from "./pages/Services";
-// import Signup from "./pages/Signup";
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import CarDetails from "./components/CarDetails"
+import Hero from "./components/Hero"
+import Home from "./Home"
+import Footer from "./components/Footer"
+// import TopIframe from ".components/TopIframe"
 
+// const url = 'http://localhost:3001/'
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Header />
-        
-        {/* <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/contactus" component={Contactus}></Route>
-          <Route path="/products" component={Products}></Route>
-          <Route path="/services" component={Services}></Route>
-          <Route path="/signup" component={Signup}></Route>
-          <Route path="/marketing" component={Marketing}></Route>
-          <Route path="/development" component={Development}></Route>
-          <Route path="/design" component={Design}></Route>
-          <Route path="/consulting" component={Consulting}></Route>
-        </Switch> */}
-      </BrowserRouter>
-    </>
-  );
+// const[open,setOpen]=useState(false)
+   return (
+      <>
+         <Router>
+            <Navbar />
+            <Header />
+         </Router>
+         {/* <TopIframe/> */}
+         <Home/>
+         {/* <Hero /> */}
+         <br /> <br /> <br /> <br /> <br /> <br />
+         <Footer />
+      </>
+      // <Router>
+      //   <div className='container'>
+      //     <Routes>
+      //       <Route path='/' element={<Home/>} />
+      //     </Routes>
+      //   </div>
+      // </Router>
+
+
+   )
 }
+
 
 export default App;
