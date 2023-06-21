87f5d69af8e9e1dd1dbab9e44ce1f11a0ae59cc7
Yonathan Mizo <ralteyonatan@yesmalot.co.il>
2023-06-21 20:10:47 +0300

    Update Routes & Model

--- backend/model/carModel.js
+++ backend/model/carModel.js
@@ -1,28 +1,22 @@
-const  mongoose = require('mongoose')
-const schema = mongoose.Schema
-
-const userSchema = mongoose.Schema({
-    uname: {
-        type: String,
-        required: [true, 'Please add a username']
-    },
-    contact: {
-        type: String,
-        required: [true, 'Please add your number']
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        required: [true, 'Please add your email']
-    },
-})
+const mongoose = require('mongoose')
+const User = require('./userModel');
 
 const carSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+
+    avatar: {
+        type: String
+    },
+
     images: [String],
 
     name: {
         type: String,
-        required: [true, 'Please add a car name']
+        required: [true, 'Please add a name']
     },
     info: {
         type: String,
@@ -49,117 +43,123 @@ const carSchema = mongoose.Schema({
         required: [true, 'Please add a description']
     },
 
-    user: userSchema,
-
     salesArea: {
         type: String,
         required: [true, 'Please add a description']
     },
-        mileAge: {
-            type: Number,
-            required: [true, 'Please add a mileage']
-        },
-        engineType: {
-            type: String,
-            required: [true, 'Please add a engine type']
-        },
-        gearbox: {
-            type: String,
-            required: [true, 'Please add a gearbox']
-        },
-        color: {
-            type: String,
-            required: [true, 'Please add a color']
-        },
-        firstDay: {
-            type: Date,
-            required: [true, 'Please add a first day on the road']
-        },
-        lastDay: {
-            type: Date,
-            required: [true, 'Please add a last day on the road']
-        },
-        currentOwnership: {
-            type: String,
-            required: [true, 'Please add a current ownership']
-        },
-        previousOwnership: {
-            type: String,
-            required: [true, 'Please add a previous ownership']
-        }, 
-        airConditioner: {
-            type: Boolean,
-            default: false
-        },
-        roofWindow: {
-            type: Boolean,
-            default: false
-        },
-        fourIntoTwo: {
-            type: Boolean,
-            default: false
-        },
-        stabilityControl: {
-            type: Boolean,
-            default: false
-        },
-        tirePressureSensors: {
-            type: Boolean,
-            default: false
-        },
-        identifyTrafficSign: {
-            type: Boolean,
-            default: false
-        },
-        abs: {
-            type: Boolean,
-            default: false
-        },
-        powerSteering: {
-            type: Boolean,
-            default: false
-        },
-        reverseCamera: {
-            type: Boolean,
-            default: false
-        },
-        pedestrianDetection: {
-            type: Boolean,
-            default: false
-        },
-        beltSensors: {
-            type: Boolean,
-            default: false
-        },
-        electricWindows: {
-            type: Boolean,
-            default: false
-        },
-        airBags: {
-            type: Boolean,
-            default: false
-        },
-        magnesiumWheels: {
-            type: Boolean,
-            default: false
-        },
-        auxiliaryBrakeSystem: {
-            type: Boolean,
-            default: false
-        },
-        automaticLightning: {
-            type: Boolean,
-            default: false
-        },
-        contamination: {
-            type: Number,
-            min: 1,
-            max: 15
-        }
+
+    mileAge: {
+        type: Number,
+        required: [true, 'Please add a mileage']
+    },
+    engineType: {
+        type: String,
+        required: [true, 'Please add a engine type']
+    },
+    gearbox: {
+        type: String,
+        required: [true, 'Please add a gearbox']
+    },
+    color: {
+        type: String,
+        required: [true, 'Please add a color']
+    },
+    firstDay: {
+        type: String,
+        required: [true, 'Please add a first day on the road']
+    },
+    //  firstDay: {
+    //     type: Date,
+    //     required: [true, 'Please add a first day on the road'],
+    //     get: (value) => value.toLocaleDateString('en-US', {
+    //       timeZone: 'UTC'
+    //     })
+//   },
+    
+    lastDay: {
+        type: String,
+        required: [true, 'Please add date of next test']
+    },
+    currentOwnership: {
+        type: String,
+        required: [true, 'Please add a current ownership']
+    },
+    previousOwnership: {
+        type: String,
+        required: [true, 'Please add a previous ownership']
+    },
+
+
+    airConditioner: {
+        type: Boolean,
+        default: false
+    },
+    roofWindow: {
+        type: Boolean,
+        default: false
+    },
+    fourIntoTwo: {
+        type: Boolean,
+        default: false
+    },
+    stabilityControl: {
+        type: Boolean,
+        default: false
+    },
+    tirePressureSensors: {
+        type: Boolean,
+        default: false
+    },
+    identifyTrafficSign: {
+        type: Boolean,
+        default: false
+    },
+    abs: {
+        type: Boolean,
+        default: false
+    },
+    powerSteering: {
+        type: Boolean,
+        default: false
+    },
+    reverseCamera: {
+        type: Boolean,
+        default: false
+    },
+    pedestrianDetection: {
+        type: Boolean,
+        default: false
+    },
+    beltSensors: {
+        type: Boolean,
+        default: false
+    },
+    electricWindows: {
+        type: Boolean,
+        default: false
+    },
+    airBags: {
+        type: Boolean,
+        default: false
+    },
+    magnesiumWheels: {
+        type: Boolean,
+        default: false
+    },
+    auxiliaryBrakeSystem: {
+        type: Boolean,
+        default: false
+    },
+    automaticLightning: {
+        type: Boolean,
+        default: false
+    },
+
+
 },
-{
-    timestamps: true,
-}
+    {
+        timestamps: true,
+    }
 )
 
-module.exports = mongoose.model('Car', carSchema)
+module.exports = mongoose.model('Cardata', carSchema)
--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const { getCars, 
-    getCar,
+const { getCars,
+    getOneCar, 
     setCar,
     updateCar,
     deleteCar,
     } = require('../controllers/carController')
 
-router.route('/').get(getCars).post(setCar)
-// router.get('/', getCar)
-// router.post('/', setCar)
+router.get('/', getCars)
 
-router.route('/:id').delete(deleteCar).put(updateCar).get(getCar)
-// router.put('/:id', updateCar)
-// router.delete('/:id', deleteCar)
+router.get('/:id', getOneCar)
+router.post('/', setCar)
+
+router.put('/:id', updateCar)
+router.delete('/:id', deleteCar)
+
+
 
 module.exports = router
