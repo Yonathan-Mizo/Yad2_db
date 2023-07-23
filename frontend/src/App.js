a3ac1412a2b07302e28800b5d94d5ecfb78f4bd6
Yonathan Mizo <ralteyonatan@yesmalot.co.il>
2023-07-23 20:08:37 +0300

    new updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Header from './components/Header'
 
-// import TopIframe from ".components/TopIframe"
+import Footer from "./components/Footer"
+import Header from './components/Header'
+import './index.css'
 
 // const url = 'http://localhost:3001/'
 function App() {
-// const[open,setOpen]=useState(false)
+
    return (
-      
+      <>
          <Router>
-            <Routes>
-            <Navbar />
-            <Header />
-            </Routes>
+         <Navbar /> 
+         <Header/>
          </Router>
-        
-         
+         {/* <Main />  <br /> <br /> */}
+         <Footer />
+      </>
+
+      // <Router>
+      //   <div className='container'>
+      //     <Routes>
+      //       <Route path='/' element={<Home/>} />
+      //     </Routes>
+      //   </div>
+      // </Router>
+
 
    )
 }
 
 
-
 export default App;
-
-
